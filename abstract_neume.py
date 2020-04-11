507b11017fa46f6270b49e4bca8d8eda72025ca5
Anastasios Darian <zkdarian@gmail.com>
2020-04-10 23:15:30 -0700

    plotting out abstract names, abstract_neume temp fix

--- abstract_neume.py
+++ abstract_neume.py
@@ -3,7 +3,6 @@
 from reportlab.pdfbase import pdfmetrics
 from neume import Neume
 
-
 class AbstractNeume(Neume):
     def __init__(self, char: str, font_family: str, font_size: int, color: str, offset_x: int = 0, offset_y: int = 0):
         self.char: str = char  # The character in a TTF
@@ -14,7 +13,6 @@ class AbstractNeume(Neume):
         self.color = color
         self.offset_x = 0
         self.offset_y = 0
-        self.width = 0
-        ascent = 0
-        descent = 0
-        self.height = 0
+        self.width = pdfmetrics.stringWidth(self.char, self.font_family, self.font_size) # Eventually make these into junk values by that 
+        ascent, descent = pdfmetrics.getAscentDescent(self.font_family, self.font_size)  # I mean just set them to zero, else it will set 
+        self.height = ascent - descent                                                   # the ength of the abstract identifier (useless)
