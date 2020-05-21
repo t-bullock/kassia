75e51821cb6c0534b247a6e725b8b01c8d581ca9
Anastasios Darian <zkdarian@gmail.com>
2020-05-20 21:10:22 -0700

    fix range(384); reduce code columns; don't truncate rows

--- org/adoctablescripts.py
+++ org/adoctablescripts.py
@@ -4,9 +4,9 @@ import sys
 def find_pos_of_nth_substr(in_string, num, substr):
   pos=-1
   iter=0
-  while(iter<num):
+  while iter<num and iter != -1:
     pos=in_string.find(substr,pos+1)
-    if(in_string[pos-1] != '\\'):
+    if in_string[pos-1] != '\\':
       iter+=1
   return(pos)
 
@@ -18,26 +18,39 @@ def adoc_table_to_orgtbl(in_string, delimiter):
   table_end    = in_string.find('|===',table_begin+1)
   in_string    = in_string[table_begin:table_end]
   out_string   = ''
-  for f in range(384):
-    index        = find_pos_of_nth_substr(
+
+  while True:
+    index = find_pos_of_nth_substr(
       in_string,
       field_count+1,
       delimiter)
     out_string += in_string[0:index] + '\n'
     in_string   = in_string[index:]
+    if index == -1:
+      break
+
   return(out_string)
 
 
 def get_column(in_string, column, delimiter):
-  in_string  = adoc_table_to_orgtbl(in_string, delimiter).splitlines()
+  in_string  = adoc_table_to_orgtbl(in_string,
+                                    delimiter).splitlines()
   out_string = ''
+
   for f in range(len(in_string)):
     cur_lin = in_string[f]
-    beg_col = find_pos_of_nth_substr(cur_lin, column+1, delimiter)+1
-    end_col = find_pos_of_nth_substr(cur_lin, column+2, delimiter)
+    beg_col = find_pos_of_nth_substr(cur_lin,
+                                     column+1,
+                                     delimiter)+1
+    end_col = find_pos_of_nth_substr(cur_lin,
+                                     column+2,
+                                     delimiter)
     in_string[f] = cur_lin[beg_col:end_col]
-    out_string += re.sub('\\\\\\' + delimiter, delimiter, in_string[f] + '\n')
-  return(out_string.rstrip())
+    out_string += re.sub('\\\\\\' + delimiter,
+                         delimiter,
+                         in_string[f] + '\n')
+
+  return(out_string.rstrip('\n'))
 
 
 stdin = sys.stdin.read()
