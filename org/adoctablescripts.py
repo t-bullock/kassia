fe8afe6c162aa5b6c5bdeb4ff7f2caeb79f01cd1
Anastasios Darian <zkdarian@gmail.com>
2020-05-30 15:56:59 -0700

    indent 2->4; fix bug with escaped delimiter; stop mutating function arguments

--- org/adoctablescripts.py
+++ org/adoctablescripts.py
@@ -2,77 +2,77 @@ import re
 import sys
 
 def is_escaped(in_string, char_pos):
-  return( in_string[char_pos-1] == '\\'
-          and not is_escaped(in_string, char_pos-1))
-
-def find_pos_of_nth_substr(in_string, num, substr):
-  pos=-1
-  i=0
-  while i<num:
-    pos=in_string.find(substr,pos+1)
-    if not is_escaped(in_string, pos):
-      i+=1
-    elif pos == -1:
-      break
-  return(pos)
+    return( in_string[char_pos-1] == '\\'
+            and not is_escaped(in_string, char_pos-1))
 
 
-def adoc_table_to_orgtbl(in_string, delimiter, field_count):
-  in_string    = re.sub('\n',' ', in_string)
-  table_begin  = in_string.find('|===',0)+4
-  table_end    = in_string.find('|===',table_begin+1)
-  in_string    = in_string[table_begin:table_end]
-  out_string   = ''
+def find_nth_substr(in_string, num, substr):
+    def inner_func(in_string, num, substr, pos, i):
+        new_pos = in_string.find(substr, pos if pos==i==0 else pos+1)
+        new_i   = i if is_escaped(in_string, pos) else i+1
+        if i >= num and new_i != i or pos == -1:
+            return pos
+        return inner_func(in_string, num, substr, new_pos, new_i)
+    return inner_func(in_string, num, substr, 0, 0)
 
-  index = 0
-  while index != -1:
-    index = find_pos_of_nth_substr(
-      in_string,
-      field_count+1,
-      delimiter)
-    out_string += in_string[0:index] + '\n'
-    in_string   = in_string[index:]
 
-  return(out_string)
+def arrange_in_columns(in_string, delimiter, field_count):
+    intermediate_string    = re.sub('\n',' ', in_string)
+    table_begin            = intermediate_string.find('|===',0)+4
+    table_end              = intermediate_string.find('|===',table_begin+1)
+    intermediate_string    = intermediate_string[table_begin:table_end]
+    out_string             = ''
+
+    index = 0
+    while index != -1:
+        index = find_nth_substr(
+            intermediate_string,
+            field_count+1,
+            delimiter)
+        out_string += intermediate_string[0:index] + '\n'
+        intermediate_string = intermediate_string[index:]
+
+    return(out_string)
 
 
 def get_column(in_string, column, delimiter, field_count):
-  in_string  = adoc_table_to_orgtbl(in_string,
-                                    delimiter,
-                                    field_count
-                                    ).splitlines()
-  out_string = ''
+    intermediate_string = arrange_in_columns(
+            in_string,
+            delimiter,
+            field_count
+            ).splitlines()
+    out_string=''
 
-  for f in range(len(in_string)):
-    cur_lin = in_string[f]
-    beg_col = find_pos_of_nth_substr(cur_lin,
-                                     column+1,
-                                     delimiter)+1
-    end_col = find_pos_of_nth_substr(cur_lin,
-                                     column+2,
-                                     delimiter)
-    in_string[f] = cur_lin[beg_col:end_col]
-    out_string += re.sub('\\\\\\' + delimiter,
-                         delimiter,
-                         in_string[f] + '\n')
+    for f in range(len(intermediate_string)):
+        cur_lin = intermediate_string[f]
+        beg_col = find_nth_substr(cur_lin,
+                                  column+1,
+                                  delimiter)+1
+        end_col = find_nth_substr(cur_lin,
+                                  column+2,
+                                  delimiter)
+        intermediate_string[f] = cur_lin[beg_col:end_col]
+        out_string += re.sub('\\\\\\' + delimiter,
+                             delimiter,
+                             intermediate_string[f] + '\n')
 
-  return(out_string.rstrip('\n'))
+    return(out_string.rstrip('\n'))
 
 
 stdin = sys.stdin.read()
 if len(sys.argv) < 2:
-  print("Error: Missing args")
+    print("Error: Missing args")
 
 elif sys.argv[1] == "print-column" :
-  if len(sys.argv) > 2:
-    print(get_column(stdin, int(sys.argv[2]), '|', 9))
-  else:
-    print("Error: no column specified")
+    if len(sys.argv) > 2:
+        print(get_column(stdin, int(sys.argv[2]), '|', 9))
+    else:
+        print("Error: no column specified")
 
 
 elif sys.argv[1] == "print-dirty-orgtbl" :
-  print(adoc_table_to_orgtbl(stdin, '|', 9))
+    print(arrange_in_columns(stdin, '|', 9))
 
 
 else:
-  print("Error: First arg must be a command, ie 'print-column'")
+    print("Error: First arg must be a command, ie 'print-column'")
